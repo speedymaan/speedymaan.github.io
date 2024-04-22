00fd31a44e67b02c3cd17f0580f70fdb17135831
speedymaan <rolika2006@gmail.com>
2024-04-22 08:39:55 +0200

    removed old map.js

--- scripts/map.js
+++
@@ -1,5 +0,0 @@
-// Initialize the map
-var map = L.map("map").setView([51.505, -0.09], 13); // Adjust center and zoom as needed
-
-// Add the TinyWorld layer
-new L.GridLayer.TinyWorld().addTo(map);
